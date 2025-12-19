beb5a3c692c53d3da2aeaf24a3d963435dcf5596
Harsh Singh <harshsinghsingh781@gmail.com>
2025-12-19 17:52:21 +0530

    adding buysphere

---
+++ CLIENT/src/pages/admin-view/orders.jsx
@@ -0,0 +1,10 @@
+import AdminOrdersView from '@/components/admin-view/order'
+import React from 'react'
+
+function Adminorders() {
+  return (
+    <div><AdminOrdersView/></div>
+  )
+}
+
+export default Adminorders
